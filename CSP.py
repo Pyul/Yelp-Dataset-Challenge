5bcbcd1bb4d0c579bd763395e765a3da747ddc62
Christopher Heung <christopherheung@gmail.com>
2015-12-05 10:34:29 -0800

    Started working on CSP
    Added csp library and added proof of concept code

--- CSP.py
+++ CSP.py
@@ -1,1 +1,34 @@
-__author__ = 'sigberto'
+__author__ = 'chris'
+from constraint import *
+
+class CSP:
+
+    def __init__(self):
+        self.problem = Problem()
+
+    def addVariable(self, newVariable, valueRange):
+        self.problem.addVariables(newVariable, valueRange)
+
+    def addConstraint(self, lambdaFn, variables):
+        self.problem.addConstraint(lambdaFn, variables)
+
+    def solve(self):
+        solutions = self.problem.getSolutions()
+        return solutions
+
+def compareFn(row1, row2):
+    return row1 != row2
+
+csp = CSP()
+numpieces = 3
+rows = range(numpieces)
+cols = range(numpieces)
+
+print rows, cols
+csp.addVariable(cols, rows)
+
+for col1 in cols:
+    for col2 in cols:
+        if col1 < col2:
+            csp.addConstraint(compareFn, (col1, col2))
+print csp.solve()
